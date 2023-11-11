a0249271ae1b1ffe7ba59f0d816ff5dedd9abdcb
AhmedAbdAlZaher <ahmed.abdalzaher@outlook.com>
2023-11-11 12:56:29 +0200

    exvideos

--- src/components/ExerciesDetails/ExerciesDetails.jsx
+++ src/components/ExerciesDetails/ExerciesDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { exerciseOptions, fetchData } from '../../utils/fetchData'
+import { exerciseOptions, fetchData, youtubeOptions } from '../../utils/fetchData'
 import { Box } from '@mui/material'
 import Detail from '../Detail/Detail'
 import ExerciesVideos from '../ExerciesVideos/ExerciesVideos'
@@ -11,28 +11,32 @@ import { useParams } from 'react-router-dom'
 
 export default function ExerciesDetails() {
 
-  const [exeriseDetail, setExeriseDetail] = useState()
+  const [exerciseDetail, setExerciseDetail] = useState({})
+  const [exerciseVideos, setExerciseVideos] = useState([]);
 
   const { id } = useParams();
 
   useEffect(() => {
     const fetchExerisesData = async () => {
-      const exeriseDbURl = `https://exercisedb.p.rapidapi.com`
+      const exeriseDbURl = 'https://exercisedb.p.rapidapi.com';
       const youtubeSearchUrl = `https://youtube-search-and-download.p.rapidapi.com`
-
-      const exeriseDetailData = await fetchData(`${exeriseDbURl}/exersices/${id}` , exerciseOptions)
-      setExeriseDetail(exeriseDetailData)
-
+      const exeriseDetailData = await fetchData(`${exeriseDbURl}/exercises/exercise/${id}`, exerciseOptions)
+      setExerciseDetail(exeriseDetailData)
+      const exersieVideoData = await fetchData(`${youtubeSearchUrl}/search?q=${exeriseDetailData.name}`, youtubeOptions)
+setExerciseVideos(exersieVideoData)
 
     }
+
     fetchExerisesData()
-  }, [id])
+
+  }, [id]);
+
 
 
   return (
     <Box>
-      <Detail exerciseDetail={exeriseDetail} />
-      <ExerciesVideos />
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciesVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
       <SimilarExercises />
     </Box>
   )
--- src/components/ExerciesVideos/ExerciesVideos.jsx
+++ src/components/ExerciesVideos/ExerciesVideos.jsx
@@ -1,7 +1,15 @@
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 
-export default function ExerciesVideos() {
+export default function ExerciesVideos({exerciseVideos , name}) {
+
+
+
   return (
-    <div>ExerciesVideos</div>
+   <Box sx={{marginTop:{lg:'200px' ,xs:"20px"}}} p={"20px"}>
+<Typography variant='h4' mb="33px">
+Watch <span style={{color:"#ff2625", textTransform:"capitalize"}}>{name}</span> exersise videos
+</Typography>
+   </Box>
   )
 }
