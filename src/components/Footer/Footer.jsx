da68491f284fe14f8a4ead4d453fe81c7cdb6c81
AhmedAbdAlZaher <ahmed.abdalzaher@outlook.com>
2023-11-11 23:30:15 +0200

    final

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
-
+import { Box, Stack, Typography } from '@mui/material'
+import logo from '../../assets/images/Logo-1.png'
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <Box mt={"80px"} bgcolor={"#fff3f4"}>
+<Stack gap={"40px"} alignItems={'center'} px={"40px"} pt={"24px"}>
+<img src={logo} alt="log" width={"200px"} height={"40px"} />
+<Typography variant='h5' pb={"40px"} mt={"20px"}>
+  Made By Ahmed Mohamed AbdEL-Zaher
+</Typography>
+</Stack>
+    </Box>
   )
 }
